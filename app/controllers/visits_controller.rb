669645585d73c04f12389db910bea078009c3cd1
ROGER Equipaciones <rogerjunguitubriz@MacBook-Pro-de-roger-junguitu-briz.local>
2014-07-23 16:44:54 +0200

    Add create method

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -8,9 +8,30 @@ class VisitsController < ApplicationController
 		@visit = @location.visits.new
 	end
 
+	def create
+	   @location = Location.find(params[:location_id])
+	   @visit = @location.visits.new(visit_params)
+	     if @visit.save
+	        redirect_to action: 'index', controller: 'visits',
+	        location_id: @location.id
+	     else
+	     	@visits
+	        render 'new'
+	     end
+	end
+
 	def show
 		@locations = Location.find(params[:location_id])
 		@visits = @locations.visits.find(params[:id])
+			rescue ActiveRecord::RecordNotFound
+			render 'not_location_found'
 	end
+
+
+	private
+
+	    def visit_params
+		  params.require(:visit).permit(:user_name, :from_date, :to_date)
+		end
 end
 
